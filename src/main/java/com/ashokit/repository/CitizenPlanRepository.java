fb6156f11d84c16518c14c961bcdc263ed96873b
balam <bala.m15432@gmail.com>
2023-04-08 13:12:47 +0530

    Project setup

---
+++ src/main/java/com/ashokit/repository/CitizenPlanRepository.java
@@ -0,0 +1,10 @@
+package com.ashokit.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ashokit.entity.CitizenPlanEntity;
+
+public interface CitizenPlanRepository extends JpaRepository<CitizenPlanEntity, Integer> {
+
+	
+}
